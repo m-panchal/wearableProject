d8e84648e22f42d271989fcc80510aadf6dcd8a4
m-panchal <mpanchal@umass.edu>
2017-11-23 15:27:21 -0500

    add paired device and scan button

--- app/src/main/java/com/example/disha/wearableproject/BluetoothActivity.java
+++ app/src/main/java/com/example/disha/wearableproject/BluetoothActivity.java
@@ -16,11 +16,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Set;
 
 public class BluetoothActivity extends AppCompatActivity {
     private static final String TAG = "BluetoothActivity";
@@ -28,9 +31,12 @@ public class BluetoothActivity extends AppCompatActivity {
 
     private BluetoothAdapter mBluetoothAdapter;
 
-    private ArrayList<BluetoothDevice> mBTDevices = new ArrayList<>();
-    private DeviceListAdapter mDeviceListAdapter;
-    private ListView listView;
+    private ArrayList<BluetoothDevice> mBTNewDevices = new ArrayList<>();
+    private ArrayList<BluetoothDevice> mBTPairedDevices = new ArrayList<>();
+    private DeviceListAdapter mNewDeviceListAdapter;
+    private DeviceListAdapter mPairedDeviceListAdapter;
+    private ListView pairedListView;
+    private ListView newListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +44,16 @@ public class BluetoothActivity extends AppCompatActivity {
 
         setContentView(R.layout.device_list_view);
 
-        mDeviceListAdapter = new DeviceListAdapter(this, mBTDevices);
-        listView = (ListView) findViewById(R.id.lvNewDevices);
-        listView.setAdapter(mDeviceListAdapter);
+        mPairedDeviceListAdapter = new DeviceListAdapter(this, mBTPairedDevices);
+        pairedListView = (ListView) findViewById(R.id.lvPairedDevices);
+        pairedListView.setAdapter(mPairedDeviceListAdapter);
+        pairedListView.setOnItemClickListener(mDeviceClickListener);
+
+        mNewDeviceListAdapter = new DeviceListAdapter(this, mBTNewDevices);
+        newListView = (ListView) findViewById(R.id.lvNewDevices);
+        newListView.setAdapter(mNewDeviceListAdapter);
+        newListView.setOnItemClickListener(mDeviceClickListener);
+
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null) {
             Log.d(TAG, "onCreate: Does not have BT capabilities.");
@@ -80,6 +93,20 @@ public class BluetoothActivity extends AppCompatActivity {
 
     @TargetApi(Build.VERSION_CODES.M)
     private void listDevices() {
+        // Get a set of currently paired devices
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+        // If there are paired devices, add each one to the ArrayAdapter
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                mBTPairedDevices.add(device);
+                mPairedDeviceListAdapter.notifyDataSetChanged();
+            }
+            ((TextView) findViewById(R.id.tvNoPairedDevices)).setVisibility(View.GONE);
+        } else {
+            ((TextView) findViewById(R.id.tvNoPairedDevices)).setVisibility(View.VISIBLE);
+        }
+
         if (mBluetoothAdapter.isDiscovering()) {
             mBluetoothAdapter.cancelDiscovery();
         }
@@ -91,17 +118,21 @@ public class BluetoothActivity extends AppCompatActivity {
             this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1001); //Any number
         }
 
-        mBluetoothAdapter.startDiscovery();
-
         // Register for broadcasts when a device is discovered.
         registerReceiver(mBroadcastReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
         registerReceiver(mBroadcastReceiver, new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED));
+
+        mBluetoothAdapter.startDiscovery();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        // Don't forget to unregister the ACTION_FOUND receiver.
+        // Make sure we're not doing discovery anymore
+        if (mBluetoothAdapter != null) {
+            mBluetoothAdapter.cancelDiscovery();
+        }
+        // Don't forget to unregister the broadcast listener.
         unregisterReceiver(mBroadcastReceiver);
     }
 
@@ -115,19 +146,31 @@ public class BluetoothActivity extends AppCompatActivity {
                 // object and its info from the Intent.
                 Log.d(TAG, "onReceive: ACTION_FOUND.");
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                if (!mBTDevices.contains(device)) {
-                    mBTDevices.add(device);
-                    mDeviceListAdapter.notifyDataSetChanged();
+                // If it's already in list or paired, skip it, because it's been listed already
+                if ((!mBTNewDevices.contains(device)) && (device.getBondState() != BluetoothDevice.BOND_BONDED)) {
+                    mBTNewDevices.add(device);
+                    mNewDeviceListAdapter.notifyDataSetChanged();
                 }
             }
 
             // When discovery cycle finished
             if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
-                if (mBTDevices.isEmpty()) {
-                    ((TextView) findViewById(R.id.tvNoDevices)).setVisibility(View.VISIBLE);
+                if (mBTNewDevices.isEmpty()) {
+                    ((TextView) findViewById(R.id.tvNoNewDevices)).setVisibility(View.VISIBLE);
                 }
                 ((ProgressBar) findViewById(R.id.progressBar)).setVisibility(View.INVISIBLE);
             }
+        }
+    };
+
+    private AdapterView.OnItemClickListener mDeviceClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            mBluetoothAdapter.cancelDiscovery();
+            Toast.makeText(getApplicationContext(),"Clicked",Toast.LENGTH_SHORT).show();
+                /*Intent intent = new Intent(context, NewsItemActivity.class);
+                context.startActivity(intent);
+                Item item = (Item)listview.getAdapter().getItem(position);*/
         }
     };
 }
