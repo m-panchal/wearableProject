4d31f9bccc7ce659203c6ffdf789380bdd7f916b
m-panchal <mpanchal@umass.edu>
2017-11-30 00:28:27 -0500

    sensor collecting data

--- app/src/main/java/com/example/disha/wearableproject/TremorRestActivity.java
+++ app/src/main/java/com/example/disha/wearableproject/TremorRestActivity.java
@@ -10,6 +10,7 @@ import android.hardware.SensorManager;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -20,7 +21,9 @@ import org.w3c.dom.Text;
 
 import static android.hardware.Sensor.TYPE_ACCELEROMETER;
 import static android.hardware.Sensor.TYPE_GYROSCOPE;
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
 import static com.example.disha.wearableproject.R.id.playButton;
+import static com.example.disha.wearableproject.R.id.textView;
 
 public class TremorRestActivity extends AppCompatActivity implements SensorEventListener{
 
@@ -41,10 +44,11 @@ public class TremorRestActivity extends AppCompatActivity implements SensorEvent
     private static float mGyroscopeZ;
 
     private SQLiteDatabase mDb;
-    private SensorManager mAccelSensorManager;
-    private SensorManager mGyroSensorManager;
+    private SensorManager mSensorManager;
     private Sensor mAccelSensor;
     private Sensor mGyroSensor;
+    private Boolean done = false;
+    private Integer count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,7 +66,10 @@ public class TremorRestActivity extends AppCompatActivity implements SensorEvent
             @Override
             public void onFinish() {
                 //counter.setVisibility(View.GONE);
-                thread.interrupt();
+                //thread.interrupt();
+                synchronized (done) {
+                    done.notify();
+                }
                 counter.setText("Done!");
                 //playButton.setVisibility(View.VISIBLE);
             }
@@ -73,19 +80,48 @@ public class TremorRestActivity extends AppCompatActivity implements SensorEvent
             public void onClick(View v) {
                 playButton.setVisibility(View.GONE);
                 counter.setVisibility(View.VISIBLE);
-                countDownTimer.start();
+                //countDownTimer.start();
                 thread = new Thread(new Runnable() {
                     @Override
                     public void run() {
                         SensorDataDbHelper dbHelper = new SensorDataDbHelper(TremorRestActivity.this);
                         mDb = dbHelper.getWritableDatabase();
-                        mAccelSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-                        mAccelSensor = mAccelSensorManager.getDefaultSensor(TYPE_ACCELEROMETER);
-                        mGyroSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-                        mGyroSensor = mGyroSensorManager.getDefaultSensor(TYPE_GYROSCOPE);
-                        while(!Thread.interrupted());
-                        mAccelSensorManager.unregisterListener(TremorRestActivity.this);
-                        mGyroSensorManager.unregisterListener(TremorRestActivity.this);
+                        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+                        mAccelSensor = mSensorManager.getDefaultSensor(TYPE_ACCELEROMETER);
+                        mGyroSensor = mSensorManager.getDefaultSensor(TYPE_GYROSCOPE);
+                        mSensorManager.registerListener(TremorRestActivity.this, mAccelSensor, 20);
+                        mSensorManager.registerListener(TremorRestActivity.this, mGyroSensor, 20);
+                        Log.d("Thread", "Started");
+                        counter.post(new Runnable() {
+                            public void run() {
+                                counter.setText("Collecting\n data...");
+                            }
+                        });
+                        while (count < 20) {
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            count++;
+                        }
+                        counter.post(new Runnable() {
+                            public void run() {
+                                counter.setText("Done!");
+                            }
+                        });
+                        //counter.setText("Done!");
+                        /*synchronized (done) {
+                            try {
+                                done.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }*/
+                        //while(!Thread.interrupted());
+                        mSensorManager.unregisterListener(TremorRestActivity.this);
+                        mSensorManager.unregisterListener(TremorRestActivity.this);
+                        mDb.close();
                     }
                 });
                 thread.start();
@@ -96,10 +132,11 @@ public class TremorRestActivity extends AppCompatActivity implements SensorEvent
     @Override
     public void onSensorChanged(SensorEvent event) {
         // If sensor is unreliable, then just return
-        if (event.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE)
+        Log.d("onSensorChanged", "Received event " + String.valueOf(event.accuracy));
+        /*if (event.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE)
         {
             return;
-        }
+        }*/
         mMagnitude = (float)Math.sqrt((float)Math.pow(event.values[0],2)+(float)Math.pow(event.values[1],2)+(float)Math.pow(event.values[2],2));
 
         if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
@@ -163,6 +200,7 @@ public class TremorRestActivity extends AppCompatActivity implements SensorEvent
         cv.put(SensorDataContract.SensorDataEntry.COLUMN_GYROSCOPE_X,gyroscopeX);
         cv.put(SensorDataContract.SensorDataEntry.COLUMN_GYROSCOPE_Y,gyroscopeY);
         cv.put(SensorDataContract.SensorDataEntry.COLUMN_GYROSCOPE_Z,gyroscopeZ);
+        Log.d("addNewSensorData", "Added");
         return mDb.insert(SensorDataContract.SensorDataEntry.TABLE_NAME,null,cv);
     }
 }
